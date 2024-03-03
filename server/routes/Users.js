5d9dbdb87acdff6d9d495533fed1555f4a0e1018
Marce <assumpcaom23@gmail.com>
2024-03-03 16:06:49 -0300

    feat: token de acesso a aplicação ao fazer login

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
+const { sign } = require("jsonwebtoken");
+
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
   bcrypt.hash(password, 10).then((hash) => {
@@ -25,7 +27,12 @@ router.post("/login", async (req, res) => {
 
   bcrypt.compare(password, user.password).then((match) => {
     if (!match) res.json({ error: "Wrong username and password combination" });
-    res.json("You logged in!");
+
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    res.json(accessToken);
   });
 });
 
